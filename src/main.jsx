784931478cfbf3d88b3cd87a411f8a6e9954a98b
jYoung706 <wnsdud9476@gmail.com>
2024-07-07 01:07:18 +0900

    [Add] project router & tailwind css init

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import NotFound from "@/pages/NotFound.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Videos from "@/pages/Videos.jsx";
+import VideoDetail from "@/pages/VideoDetail.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
